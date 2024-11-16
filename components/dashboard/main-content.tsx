a77dc211bd3e58dc9b4fd9243651c1320999ad93
Krishant Timilsina <krishtimil@gmail.com>
2024-11-16 11:30:37 +0545

    fix: when no component found

--- components/dashboard/main-content.tsx
+++ components/dashboard/main-content.tsx
@@ -1,21 +1,40 @@
-import Home from "@/components/dashboard/contents/home";
-import Explore from "@/components/dashboard/contents/explore";
+import { lazy, Suspense, useState, useEffect } from "react";
 
 export default function MainContent({
-  collapsed,
-  active,
+    collapsed,
+    active,
 }: {
-  collapsed: boolean;
-  active: string;
+    collapsed: boolean;
+    active: string;
 }) {
-  return (
-    <div className={`${collapsed ? "col-span-9" : "col-span-8"} border-x`}>
-      <div className="sticky top-0 backdrop-blur-md bg-white/80 border-b p-4 text-black">
-        <h1 className="text-xl font-bold">{active}</h1>
-      </div>
+    const [Component, setComponent] = useState<React.ComponentType | null>(null);
 
-      {active == "Home" && <Home />}
-      {active == "Explore" && <Explore/>}
-    </div>
-  );
+    useEffect(() => {
+        const loadComponent = async () => {
+            try {
+                const DynamicComponent = await import(
+                    `@/components/dashboard/contents/${active.toLowerCase()}`
+                );
+                setComponent(() => DynamicComponent.default);
+            } catch {
+                const NotFound = () => { return <div className="text-black p-4 flex items-center justify-center">404 Not Found.</div>; };
+
+                setComponent(() => NotFound);
+            }
+        };
+
+        loadComponent();
+    }, [active]);
+
+    return (
+        <div className={`${collapsed ? "col-span-9" : "col-span-8"} border-x`}>
+            <div className="sticky top-0 backdrop-blur-md bg-white/80 border-b p-4 text-black">
+                <h1 className="text-xl font-bold">{active}</h1>
+            </div>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                {Component ? <Component /> : <div>Loading...</div>}
+            </Suspense>
+        </div>
+    );
 }
